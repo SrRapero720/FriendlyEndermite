df2c90eccbaecd07ac1616f35ad44810a1171dc5
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-03-01 17:03:19 +0100

    Formatted entire project

--- src/main/java/team/creative/friendlyendermite/FriendlyEndermite.java
+++ src/main/java/team/creative/friendlyendermite/FriendlyEndermite.java
@@ -16,29 +16,29 @@ import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 @Mod.EventBusSubscriber
 @Mod(value = FriendlyEndermite.modid)
 public class FriendlyEndermite {
-	
-	public static final String modid = "friendlyendermite";
-	public static final String version = "1.0";
-	
-	private static final Field goals = ObfuscationReflectionHelper.findField(GoalSelector.class, "field_220892_d");
-	private static final Field targetClassField = ObfuscationReflectionHelper.findField(NearestAttackableTargetGoal.class, "field_75307_b");
-	
-	@SubscribeEvent
-	public static void entitySpawned(EntityJoinWorldEvent event) {
-		if (event.getEntity() instanceof EndermanEntity) {
-			EndermanEntity man = (EndermanEntity) event.getEntity();
-			try {
-				Set<PrioritizedGoal> set = (Set<PrioritizedGoal>) goals.get(man.targetSelector);
-				for (PrioritizedGoal goal : set) {
-					if (goal.getGoal() instanceof NearestAttackableTargetGoal && targetClassField.get(goal) == EndermiteEntity.class) {
-						man.targetSelector.removeGoal(goal.getGoal());
-						return;
-					}
-				}
-			} catch (IllegalArgumentException | IllegalAccessException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
+    
+    public static final String modid = "friendlyendermite";
+    public static final String version = "1.0";
+    
+    private static final Field goals = ObfuscationReflectionHelper.findField(GoalSelector.class, "field_220892_d");
+    private static final Field targetClassField = ObfuscationReflectionHelper.findField(NearestAttackableTargetGoal.class, "field_75307_b");
+    
+    @SubscribeEvent
+    public static void entitySpawned(EntityJoinWorldEvent event) {
+        if (event.getEntity() instanceof EndermanEntity) {
+            EndermanEntity man = (EndermanEntity) event.getEntity();
+            try {
+                Set<PrioritizedGoal> set = (Set<PrioritizedGoal>) goals.get(man.targetSelector);
+                for (PrioritizedGoal goal : set) {
+                    if (goal.getGoal() instanceof NearestAttackableTargetGoal && targetClassField.get(goal) == EndermiteEntity.class) {
+                        man.targetSelector.removeGoal(goal.getGoal());
+                        return;
+                    }
+                }
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
 }
